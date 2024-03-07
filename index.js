bac58b35b29d5eba914c7a108c448633ddfc2747
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2024-03-07 11:16:23 -0500

    fix cors origin

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const connectMongo = async () => {
 }
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    // origin:'http://localhost:3000', 
+    origin: 'https://bartow-sports-zone.netlify.app',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
